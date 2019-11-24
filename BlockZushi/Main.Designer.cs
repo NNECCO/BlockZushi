7b6ee64bd959b672aee27206f1f79c5857e87f1b
robocat <shitujinohitujikun@gmail.com>
2019-11-24 21:05:54 +0900

    191124 BlockZushi 反射板実装

---
+++ BlockZushi/Main.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace BlockZushi {
+    partial class Main {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.SuspendLayout();
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(778, 694);
+            this.DoubleBuffered = true;
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Main_Paint);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Main_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ BlockZushi/Main.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BlockZushi {
+
+    public partial class Main : Form {
+
+        private Rectangle mRect;
+        private int mSxRect, mSyRect, mWidthRect, mHeightRect;
+        private int mSpeedX;
+
+        private void Main_KeyPress(object sender, KeyPressEventArgs e) {
+            switch (e.KeyChar) {
+                case 'a':
+                    if (mRect.X - mSpeedX >= 0) mRect.X -= mSpeedX;
+                    break;
+                case 'd':
+                    if (mRect.X + mSpeedX <= Width - (mWidthRect*1.3)) mRect.X += mSpeedX;
+                    break;
+            }
+        }
+
+        public Main() {
+            InitializeComponent();
+            mSxRect = Width / 2;
+            mSyRect = Height * 4 / 5;
+            mWidthRect = 50;
+            mHeightRect = 10;
+            mRect = new Rectangle(mSxRect-mWidthRect/2, mSyRect, mWidthRect, mHeightRect);
+            mSpeedX = 5;
+        }
+
+        private void Main_Paint(object sender, PaintEventArgs e) {
+            e.Graphics.FillRectangle(Brushes.OrangeRed, mRect);
+            Invalidate();
+        }
+
+    }
+}
